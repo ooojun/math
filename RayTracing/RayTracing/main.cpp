30a5ce002440374ae349d8afa98b218cafcec02b
ooojun <ozaki_jun_123@icloud.com>
2019-12-13 19:26:14 +0900

    add

--- RayTracing/RayTracing/main.cpp
+++ RayTracing/RayTracing/main.cpp
@@ -2,6 +2,7 @@
 #include<math.h>
 #include"Geometry.h"
 #include "main.h"
+
 const int screen_width = 640;
 const int screen_height = 480;
 
@@ -12,32 +13,25 @@ const int screen_height = 480;
 ///レイ(光線)と球体の当たり判定
 ///@param ray (視点からスクリーンピクセルへのベクトル)
 ///@param sphere 球
-///@param distance 交点までの距離
 ///@hint レイは正規化しといたほうが使いやすいだろう
-bool IsHitRayAndObject(const Position3& eye, const Vector3& ray, const Sphere& sp, Vector3& Normal,Vector3 &hitPos) {
-	//レイが正規化済みである前提で…
-	//
-	//視点から球体中心へのベクトル(視線)を作ります
-	//
-	auto C = sp.pos - eye;
+bool IsHitRayAndObject(const Position3& eye, const Vector3& ray, const Sphere& sp,float& distance) {
+
+	auto sphereToRay = sp.pos - eye;													//球体の中心までのベクトル
 
 	//中心ベクトルと視線の内積をとります＝＞ベクトル長　=　射影
-	auto Plen = Dot(C, ray);			//垂線長
+	auto perLen = Dot(sphereToRay, ray);												//射影長
 
-	auto Pray = ray * Plen;			
+	auto perRay = ray * perLen;															//
 
-	auto Pvec = C - Pray;
+	auto perVec = sphereToRay - perRay;													//
 
-	if (Pvec.Magnitude() < sp.radius)
+	if (perVec.Magnitude() < sp.radius)
 	{
-		auto W = sqrt(pow(sp.radius, 2) - pow(Pvec.Magnitude(), 2));
+		auto distanceToHitPos = sqrt(pow(sp.radius, 2) - pow(perVec.Magnitude(), 2));	//W
 
-		auto distance = Pray.Magnitude() - W;
+		auto power = perRay.Magnitude() - distanceToHitPos;
 
-		Normal = (ray * distance - (sp.pos - eye)).Normalized();
-
-		hitPos = (ray * distance + eye);
-
+		distance = power;
 		return true;
 	}
 	else
@@ -96,7 +90,7 @@ struct Color
 		ret.b = max(col.b, ret.b);
 		return ret;
 	}
-	unsigned int GetColor()const {
+	unsigned int GetCol()const {
 		return DxLib::GetColor(r, g, b);
 	}
 };
@@ -105,50 +99,62 @@ Color GetCheckerColorPosition(Vector3 &as)
 {
 	if (((((int)(as.x) / 30) + (int)(as.z) / 30) % 2) == 0)
 	{
-		return Color(255, 255, 255);
+		if ((int)(as.x) > 0)
+		{
+			return Color(255, 255, 255);
+		}
+		else
+		{
+			return Color(128, 128, 128);
+		}
 	}
 	else
 	{
-		return Color(128, 128, 128);
+		if ((int)(as.x) > 0)
+		{
+			return Color(128, 128, 128);
+		}
+		else
+		{
+			return Color(255, 255, 255);
+		}
 	}
 }
 
-void RayTracing(const Position3& eye, const Sphere& sphere, const Plane& plane) {
-
+void RayTracing(const Position3& eye, const Sphere& sphere, const Plane& plane) 
+{
 	auto distancepl = Dot(eye, plane.normal) - plane.offset;
 
 	for (int y = 0; y < screen_height; ++y) {//スクリーン縦方向
 		for (int x = 0; x < screen_width; ++x) {//スクリーン横方向
+
 			//①視点とスクリーン座標から視線ベクトルを作る
 			//ここでrayを作る
-			Vector3 pos = { static_cast<float>(x - screen_width / 2), -static_cast<float>(y - screen_height / 2) , 0 };
+			Vector3 pos = { static_cast<float>(x - screen_width / 2),static_cast<float>(screen_height / 2 - y),0 };
 			Vector3 ray = pos - eye;
-			
-			Vector3 normal;
-			Vector3 hitPos;
 
 			//②正規化をする
 			ray.Normalize();
+
+			auto distance = 0.0f;
+
+			auto Light = Vector3(1, -1, -1);
+			Light.Normalize();
+
 			//③IsHitRay関数がTrueだったら塗りつぶす
 			//※塗りつぶしはDrawPixel関数を使う。
-
-			if (IsHitRayAndObject(eye, ray, sphere, normal, hitPos))
+			if (IsHitRayAndObject(eye, ray, sphere,distance))
 			{
+				auto normal = (ray * distance - (sphere.pos - eye)).Normalized();
+				// 光線と球の法線ベクトルとの反射ベクトル
+				auto refLight = ReflectVector(Light, normal);
+				//照り返しをなくす為のClamp
+				float spec = pow(Clamp(Dot(refLight, -ray)), 15.0f);
 
-				int Color = 255;
-				auto Light = Vector3(1, -1, -1);
-				Light.Normalize();
-
-				// 反射ベクトル
-				auto rLight = ReflectVector(Light, normal);
-
-				// pow関数 pow(元の数,乗数)
-				float spec = pow(Clamp(Dot(rLight, -ray.Normalized())), 15.0f);
-
-				auto deff = Dot(normal, rLight);
+				auto deff = Dot(normal, -Light);
 				deff = max(deff + spec, 0.25f);
 
-				auto difCol = Color::Color(100, 255, 100);
+				auto difCol = Color::Color(255, 0, 0);
 				auto speCol = Color::Color(255, 255, 255);
 				auto ambCol = Color::Color(32, 32, 32);
 				auto col = Color::Color();
@@ -156,33 +162,34 @@ void RayTracing(const Position3& eye, const Sphere& sphere, const Plane& plane) 
 				difCol *= deff;
 				speCol *= spec;
 
-				auto tmpRay = ReflectVector(ray, normal);	//反射ベクトル
-
-				auto t = (Dot(hitPos, plane.normal) - plane.offset) / Dot(-tmpRay, plane.normal);
-
-				auto Tpos = (hitPos + tmpRay * t);
-
-				auto h = (hitPos.y - plane.offset) / 100;
-
 				col = difCol + speCol;
 				col = col.Max(ambCol);
 
-				if (Dot(ray, plane.normal) > 0)
+				auto tmpPos = ray * distance + eye;
+
+				auto tmpRay = ReflectVector(ray, normal);	//反射ベクトル
+
+				auto t = (Dot(tmpPos, plane.normal) - plane.offset) / Dot(-tmpRay, plane.normal);
+
+				if (Dot(-tmpRay, plane.normal) > 0)
 				{
-						//交点座標を求める
-						auto color = GetCheckerColorPosition(Tpos);
+					auto Tpos = tmpPos + tmpRay * t;
+					//交点座標を求める
+					auto color = GetCheckerColorPosition(Tpos);
 
-						//DrawPixel(x, y, GetColor(h * 255, h * 255, h * 255));
+					color.r /= 255.0f;
+					color.g /= 255.0f;
+					color.b /= 255.0f;
 
-						DrawPixel(x, y, color.GetColor());
-						continue;
+					DrawPixel(x, y, GetColor(col.r * color.r, col.g * color.g, col.b * color.b));
+					continue;
 				}
-	
+
 				DrawPixel(x, y, GetColor(col.r, col.g, col.b));
 			}
 			else
 			{
-				if (Dot(ray, plane.normal) > 0)
+				if (Dot(-ray, plane.normal) > 0)
 				{
 					auto rayplane = Dot(-ray, plane.normal);
 
@@ -190,21 +197,21 @@ void RayTracing(const Position3& eye, const Sphere& sphere, const Plane& plane) 
 
 					auto intercepl = eye + ray * t;
 
-					DrawPixel(x, y, GetCheckerColorPosition(intercepl).GetColor());
+					DrawPixel(x, y, GetCheckerColorPosition(intercepl).GetCol());
 
-					if (/*球体との当たり判定があったら影の描画に入る*/true)
+					//影の実装
+					if (IsHitRayAndObject(intercepl, -Light, sphere,distance))
 					{
-
+						auto col = GetCheckerColorPosition(intercepl);
+						col.r *= 0.5;
+						col.g *= 0.5;
+						col.b *= 0.5;
+						DrawPixel(x, y, col.GetCol());
 					}
 				}
 			}
 		}
 	}
-}
-
-void DrawPixelWithFloat(int x, int y, float r, float g, float b)
-{
-	DrawPixel(x, y, GetColor(r * 0xff, g * 0xff, b * 0xff));
 }
 
 int main() {
@@ -213,7 +220,7 @@ int main() {
 	SetMainWindowText(_T("1816007_尾崎　隼"));
 	DxLib_Init();
 	DrawBox(0, 0, screen_width, screen_height, GetColor(200,200,255), true);
-	RayTracing(Vector3(0, 0, 300), Sphere(100, Position3(0, 0, -100)),Plane(Position3(0, -1, 0), -100));
+	RayTracing(Vector3(0, 0, 300), Sphere(100, Position3(0, 0, -100)),Plane(Position3(0, 1, 0), -100));
 	WaitKey();
 	DxLib_End();
 }
